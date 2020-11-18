f39609858fc7501c565d36b3ebc6cfcffd2c4b09
DEH9l <www.deh9l@gmail.com>
2020-11-18 10:29:08 +0200

    JQ done

---
+++ js/My.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+ $(".js-click").click(function(){
+     $(this).toggleClass("transform");
+     $(".toogle").slideToggle("fast");
+ });
+
+    $(".js-click1").click(function(){
+        $(this).toggleClass("transform");
+        $(".toogle1").slideToggle("fast");
+    });
+});
